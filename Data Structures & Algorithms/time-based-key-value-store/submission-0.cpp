6e2aecfb2143a2415fe6631bf6b362c43552cdbf
supersanchayrx <grindinforvalore@gmail.com>
2026-04-18 23:34:27 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,42 @@
+class TimeMap {
+
+    unordered_map<string,vector<pair<string,int>>> mp;
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({value,timestamp});
+    }
+    
+    string get(string key, int timestamp) {
+        
+        int left = 0;
+        int right = mp[key].size()-1;
+
+        auto arr = mp[key];
+
+        while(left<=right)
+        {
+            int mid = (left+right)/2;
+
+            if(arr[mid].second==timestamp)
+                return arr[mid].first;
+            
+            else if(arr[mid].second>timestamp)
+            {
+                right = mid-1;
+            }
+            else
+            {
+                left=mid+1;
+            }
+        }
+
+        if(right>=0)
+            return arr[right].first;
+
+        return "";
+    }
+};
